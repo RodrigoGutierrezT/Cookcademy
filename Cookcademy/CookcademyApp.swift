4f73dfafdde5447805a942c2c4a04caa3babde11
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-03-11 08:34:58 +0900

    Initial commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Rodrigo on 09-03-24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
